f6802d40ca558f3392eef2e098b37e8f53a946de
positiveneutron <D10815802@mail.ntust.edu.tw>
2020-04-24 17:45:08 +0700

    little bit typo for commenting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const https = require("https") 
 const dotenv = require("dotenv");
 
-// envrironment configuration and caller
+// environment configuration and caller
 dotenv.config();
 const { 
     BASE_URL
